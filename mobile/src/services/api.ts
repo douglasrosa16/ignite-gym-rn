e32dafd1269fa0a650fb32eca6ccbb7abbaab9df
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2023-10-15 09:19:43 -0400

    feat: Toast, Interceptor e AppError

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,25 +1,36 @@
 import axios from 'axios';
+import { AppError } from '@utils/AppError';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.102:3333'
+  baseURL: 'http://192.168.0.102:3333',
+  timeout: 6000
 });
 
-// Interceptando as requisições
-api.interceptors.request.use((config) => {
-  console.log("INTERCEPTOR REQUEST", config.data)
-  return config;
-}, (error) => {
-  console.log("ERROR REQUEST", error);
-  return Promise.reject(error);
-});
-
-// Interceptando as respostas
-api.interceptors.response.use((response) => {
-  console.log('INTERCEPTOR RESPONSE => ', response)
-  return response;
-}, (error) => {
-  console.log('ERROR RESPONSE => ', error)
-  return Promise.reject(error);
+api.interceptors.response.use(response => response, error => {
+  if(error.response && error.response.data){
+    return Promise.reject(new AppError(error.response.data.message));
+  }else {
+    return Promise.reject(error);
+  }
 });
 
 export { api };
+
+// Interceptando as requisições
+// api.interceptors.request.use((config) => {
+//   console.log("INTERCEPTOR REQUEST", config.data)
+//   return config;
+// }, (error) => {
+//   console.log("ERROR REQUEST", error);
+//   return Promise.reject(error);
+// });
+
+// Interceptando as respostas
+// api.interceptors.response.use((response) => {
+//   console.log('INTERCEPTOR RESPONSE => ', response)
+//   return response;
+// }, (error) => {
+//   console.log('ERROR RESPONSE => ', error)
+//   return Promise.reject(error);
+// });
+
