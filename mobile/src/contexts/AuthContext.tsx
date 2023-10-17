d4c1e55b5948ea51fa989a013dfe86c79f63ac6c
douglasrosa16 <douglasoliveira163425@gmail.com>
2023-10-17 07:11:28 -0400

    feat: finalizando JWT

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
 
-import { storageAuthTokenSave, storageAuthTokenGet } from '@storage/storageAuthToken';
+import { storageAuthTokenSave, storageAuthTokenGet, storageAuthTokenRemove } from '@storage/storageAuthToken';
 import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser';
 
 import { api } from '@services/api';
@@ -63,7 +63,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true);
       setUser({} as UserDTO);
       await storageUserRemove();
-
+      await storageAuthTokenRemove();
     } catch (error) {
       throw error;
     } finally {
