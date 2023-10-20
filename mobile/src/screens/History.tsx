b32d34ca240ec9a35256049cfec1108864db4f47
douglasrosa16 <douglasoliveira163425@gmail.com>
2023-10-20 07:38:36 -0400

    feat: listando history

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -1,38 +1,61 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-import { Heading, VStack, SectionList, Text } from 'native-base';
+import { Heading, VStack, SectionList, Text, Toast, useToast } from 'native-base';
+
+import { AppError } from '@utils/AppError';
+import { api } from '@services/api';
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
+import { useFocusEffect } from '@react-navigation/native';
 
 export function History() {
-  const [exercises, setExercises] = useState([
-    {
-      title: '26.08.22',
-      data: ["Puxada frontal", "Remada unilateral"]
-    },
-    {
-      title: '27.08.22',
-      data: ["Puxada frontal"]
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([]);
+
+  const toast = useToast();
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true);
+      const response = await api.get('/history');      
+      setExercises(response.data)
+
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possivel carregar o histórico.';
+
+      toast.show({
+        title: title,
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setIsLoading(false);
     }
-  ]);
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchHistory();
+  }, []))
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
       <SectionList
         sections={exercises}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <HistoryCard />
+          <HistoryCard data={item} />
         )}
         renderSectionHeader={({ section }) => (
-          <Heading color="gray.200" fontSize="md" mt={10} mb={3}  fontFamily="heading">
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
             {section.title}
           </Heading>
         )}
         px={8}
-        contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
         ListEmptyComponent={() => (
           <Text color="gray.100" textAlign="center">
             Não há exercícios registrados ainda. {'\n'}
