74050831dbd96e89545d4c935b2d54ab17334395
douglasrosa16 <douglasoliveira163425@gmail.com>
2023-10-27 13:05:53 -0400

    feat: refreshtoken

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,13 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosError } from 'axios';
 import { AppError } from '@utils/AppError';
+import { storageAuthTokenGet } from '@storage/storageAuthToken';
 
 type SignOut = () => void;
+
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+}
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
@@ -12,13 +18,26 @@ const api = axios.create({
   timeout: 6000
 }) as APIInstanceProps;
 
+let failedQueue : Array<PromiseType> = [];
+
 //Pegando o signOut passado como parâmetro
 api.registerInterceptTokenManager = signOut => {
-  const interceptTokenManager = api.interceptors.response.use(response => response, requestError => {
+  const interceptTokenManager = api.interceptors.response.use(response => response, async ( requestError ) => {
 
     //Não autorizado
     if (requestError?.response?.status === 401) {
       if (requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+        const { refresh_token } = await storageAuthTokenGet();
+
+        //Caso não tenha o refresh Token desconecta o usuário
+        if (!refresh_token) {
+          signOut();
+          Promise.reject(requestError);
+        }
+
+        const originalRequestConfig = requestError.config;
+        console.log("REQUISIÇÃO => ", originalRequestConfig);
+
 
       }
 
